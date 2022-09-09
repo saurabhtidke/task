46d4b83ab79d7dc869d60a218d869978cb888bf0
saurabhtidke <84722951+saurabhtidke@users.noreply.github.com>
2022-09-09 12:36:02 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Login } from "./Componants/Login";
+import { Navbar } from "./Componants/Navbar";
+import { Register } from "./Componants/Register";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard } from "@mui/icons-material";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+
+        {/* Routing componants and paths */}
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
